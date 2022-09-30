5ffb37933def014529add82b26a8a1fd0781b871
brijesh <brijesh.nimap@grab.in>
2022-09-30 09:29:11 +0530

    v

--- bin/mg-create-project.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import {cli} from '../src/cli'
-console.log(cli);
-cli(process.argv)
-
-// require = require('esm')(module /*, options*/);
-
-// require('../src/cli').cli(process.argv);
-
