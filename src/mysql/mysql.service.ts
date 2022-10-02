23d5f2944d12a89e446d794bcc1dea4fbe257cea
brijesh <brijesh.nimap@grab.in>
2022-10-02 19:20:05 +0530

    initial

--- src/mysql/mysql.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class MysqlService {
-  async saveInMysql(newData: any): Promise<any> {
-    const { data, type } = newData;
-    console.log(data, type);
-  }
-}
